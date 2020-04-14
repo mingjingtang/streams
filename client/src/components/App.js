7780aec9caf2664ac6be33348ebd963c94d00f12
mingjing tang <tmjcsh@gmail.com>
2020-04-14 19:00:32 -0400

    router setting

---
+++ client/src/components/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const PageOne = () => {
+  return (
+    <div>
+      PageOne
+      <Link to="/pagetwo">Navigate to page two</Link>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      <button>
+        <Link to="/">clickMe </Link>
+      </button>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pagetwo" component={PageTwo} />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
